e75485073819368e0b1a348390303ddf20e69a5c
hta2810 <huynhthienanh2810@gmail.com>
2022-04-14 17:06:22 +0700

    thienanh

--- modules/hub/hub.tf
+++ modules/hub/hub.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "hub-vnet" {
   name                      = "${var.hub-name}"
   location                  = var.location
   resource_group_name       = var.resource_group_name
-  address_space             = var.hub-vnet-add
+  address_space             = [var.hub-vnet-add]
   # address_space             = ["${var.address_space["address"]}/${var.address_space["prefix"]}"]
 
   tags = {
